9ee80e9b23229877391ab34198fe8fca2375e9b2
bprasanna <prezna@gmail.com>
2013-11-07 15:17:12 +0530

    curl post

--- handledata.php
+++ handledata.php
@@ -3,6 +3,25 @@ header('Content-type: text/html; charset=utf-8');
 $titleVal = pg_escape_string($_POST["titleIn"]); 
 $urlVal = pg_escape_string($_POST["urlIn"]); 
 
-echo $titleVal;
+// Get cURL resource
+//Referred: http://codular.com/curl-with-php
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'https://ephemurl.firebaseio.com/urls.json',
+    CURLOPT_USERAGENT => 'Posting data using cURL',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => array(
+        title => $titleVal,
+        url => $urlVal
+    )
+));
+
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+
+// Close request to clear up some resources
+curl_close($curl);
 
 ?>
