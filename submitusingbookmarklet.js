6545873825182795414270ceafb663c248b4520b
bprasanna <prezna@gmail.com>
2013-11-07 13:33:36 +0530

    json model

--- submitusingbookmarklet.js
+++ submitusingbookmarklet.js
@@ -1,23 +1,58 @@
-function() {
-var i=document.createElement('iframe');
-i.setAttribute('name', 'ipb295319');
-i.setAttribute('id', 'ipb295319');
-i.setAttribute('allowtransparency', 'true');
-i.setAttribute('style', 'border: 0; width: 1px; height: 1px; position: absolute; left: 0; top: 0;');
-document.body.appendChild(i);
-var titleIn = e(titleIn).replace(/'/g, '%27');
-var urlIn = e(urlIn).replace(/'/g, '%27');
-window.frames['ipb295319'].document.write(
-'<html><body style="background-color: transparent;">' +
-'<form action="http://ephemurl.herokuapp.com/index.php" method="get" id="f" accept-charset="utf-8">' +
-'<input type="hidden" name="titleIn" id="titleIn" value=""/>' +
-'<input type="hidden" name="urlIn" id="urlIn" value=""/>' +
-'<input type="hidden" name="id" value="ipb295319"/>' +
+function ephemurlit(){
+    var xmlHttp=null;
+    if (window.XMLHttpRequest) {
+        xmlHttp = new window.XMLHttpRequest;
+    }
+    else {
+        try {
+            xmlHttp =  new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch(ex) {
+            xmlHttp = null;
+        }
+    }
+    var url = "http://ephemurl.herokuapp.com/handleData.php";
+    
+    var titleIn=encodeURIComponent(document.title);
+    var urlIn=encodeURIComponent(window.location.host+window.location.pathname);
+    
 
-'</form>' +
-"<scr"+"ipt>var e=encodeURIComponent,w=window,d=document,f=d.getElementById('f');" +
-"d.getElementById('titleIn').value=decodeURIComponent('" + titleIn + "');d.getElementById('urlIn').value=decodeURIComponent('" + urlIn + "');" +
-"d.getElementById('f').submit();" +
-"</scr"+"ipt></body></html>"
-);
+    //Evaluate the values
+    if(titleIn===''){
+        return false;       
+    }
+    
+    if(urlIn===''){
+        return false;
+    }
+    
+    //Clear the content page
+    if(xmlHttp != null){
+        xmlHttp.onreadystatechange=function(){
+            if (xmlHttp.readyState == 4)
+            { 
+                if (xmlHttp.status == 200){
+                    res = xmlHttp.responseText;
+                    if (res!=null){
+                        alert(res);
+                    } else {
+                        alert("Error while submitting url. Please try again.");
+                    }
+                } else if (xmlHttp.status == 404){
+                    alert("Request URL does not exist");
+                } else {
+                    alert("Error: status code is (2):" + xmlHttp.status);   
+                }
+            }
+        };
+        xmlHttp.open("POST",url,true);
+        xmlHttp.setRequestHeader("Content-type","application/x-www-form-urlencoded;charset=UTF-8");
+        xmlHttp.send("titleIn="+titleIn+"&urlIn="+urlIn);
+        return true;
+    } else {
+        alert('Your browser doesn\'t support AJAX');
+        return;     
+    }
 }
+
+ephemurlit();
